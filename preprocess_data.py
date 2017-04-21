f093904e97c7f6b56737992ca1f5859aee2bb7a2
piratte <piratte65@gmail.com>
2017-04-21 16:47:35 +0200

    better column work, proper navdata to commands parsing

--- preprocess_data.py
+++ preprocess_data.py
@@ -4,30 +4,56 @@ from pprint import pprint
 import pandas
 import numpy as np
 
-WINDOW_LENGTH = 20
-NAVDATA_OFFSET_MILIS = 20
+WINDOW_LENGTH = 10
+NAVDATA_OFFSET_MILIS = 10
+
+NAVDATA_COLUMNS = ['timestamp', 'state', 'battery level', 'magnetometer x', 'magnetometer y', 'magnetometer z',
+                   'pressure', 'temp', 'wind speed', 'wind angle', 'wind compensation: pitch',
+                   'wind compensation: roll', 'rotation around y', 'rotation around x', 'rotation around z',
+                   'altitude', 'velocity x', 'velocity y', 'velocity z', 'acceleration x', 'acceleration y',
+                   'acceleration z', 'motor 1 power', 'motor 2 power', 'motor 3 power', 'motor 4 power',
+                   'board timestamp']
+
+WANTED_NAV_COLUMNS = ['timestamp', 'state', 'magnetometer x', 'magnetometer y', 'magnetometer z',
+                      'rotation around y', 'rotation around x', 'rotation around z',
+                      'altitude', 'velocity x', 'velocity y', 'velocity z',
+                      'acceleration x', 'acceleration y', 'acceleration z',
+                      'motor 1 power', 'motor 2 power', 'motor 3 power', 'motor 4 power']
+
+COMMAND_COLUMNS = ['timestamp', 'left-right tilt', 'front-back tilt', 'vertical speed', 'angular speed']
 
 if __name__ == '__main__':
     if len(sys.argv) != 3 or '-h' in sys.argv or '--help' in sys.argv:
         print("Usage: %s NAVDATA_PATH COMMANDS_PATH" % sys.argv[0])
         sys.exit(1)
-    navdata = pandas.read_csv(sys.argv[1], sep='\t', header=None).rename(columns={0: "timestamp"})
-    commands = pandas.read_csv(sys.argv[2], sep='\t', header=None).rename(columns={0: "timestamp"})
+    navdata = pandas.read_csv(sys.argv[1], sep='\t', names=NAVDATA_COLUMNS, header=None, usecols=WANTED_NAV_COLUMNS)
+    commands = pandas.read_csv(sys.argv[2], sep='\t', names=COMMAND_COLUMNS, header=None)
 
+    print("CSV loaded")
+    # for WINDOW_LENGTH, NAVDATA_OFFSET_MILIS in [(10,10), (15,10), (15,15), (20,10), (20,15)]:
     # the data of our interest start with the beginning of the command data and end with the last sensor reading
-    begining_timestamp = commands.iloc[0, 0] - (commands.iloc[0, 0] % WINDOW_LENGTH) + WINDOW_LENGTH
+    min_com_timestamp = commands['timestamp'].min()
+    begining_timestamp = min_com_timestamp - (min_com_timestamp % WINDOW_LENGTH) + WINDOW_LENGTH
     nav_len = navdata.shape[0]-1
-    end_timestamp = navdata.iloc[nav_len, 0] - (navdata.iloc[nav_len, 0] % WINDOW_LENGTH)
+    max_nav_timestamp = navdata['timestamp'].max()
+    end_timestamp = max_nav_timestamp - (max_nav_timestamp % WINDOW_LENGTH)
 
+    print(begining_timestamp, end_timestamp)
     # we'll now create a average over a WINDOW_LENGTH ms time window, and we'll create a table of commands that covers
     # all the time. We assume that the commands have a sticky behavior (the command changes only when another arrives)
     joined_data = []
     new_navdata = []
     # just a debug counter
     counter = 0
-    prev_command = pandas.DataFrame({'timestamp': begining_timestamp, 1: .0, 2: .0, 3: .0, 4: .0, }, index=[begining_timestamp])
+    prev_command = pandas.DataFrame({'timestamp': begining_timestamp,
+                                     'left-right tilt': .0,
+                                     'front-back tilt': .0,
+                                     'vertical speed': .0,
+                                     'angular speed': .0, }, index=[begining_timestamp])
+
     for cur_timestamp in range(int(begining_timestamp), int(end_timestamp), WINDOW_LENGTH):
-        data_from_current_window = navdata.loc[(cur_timestamp < navdata['timestamp']) & (navdata['timestamp'] < cur_timestamp + WINDOW_LENGTH), :]
+        data_from_current_window = navdata.loc[(cur_timestamp + NAVDATA_OFFSET_MILIS < navdata['timestamp']) &
+                                               (navdata['timestamp'] < cur_timestamp + NAVDATA_OFFSET_MILIS + WINDOW_LENGTH), :]
         if data_from_current_window.shape[0] < 1: continue
         counter += 1
         # count the average over the timewindow for each column and add it to the new data
@@ -37,10 +63,10 @@ if __name__ == '__main__':
             if column == 'timestamp': continue
             new_row.append(data_from_current_window[column].mean(axis=0))
         new_navdata.append(tuple(new_row))
-        # if counter > 10: sys.exit()
 
         # get the command fiting to that timewindow
-        cur_commands = commands.loc[(cur_timestamp < commands['timestamp']) & (commands['timestamp'] < cur_timestamp + WINDOW_LENGTH), :]
+        cur_commands = commands.loc[(cur_timestamp < commands['timestamp']) &
+                                    (commands['timestamp'] < cur_timestamp + WINDOW_LENGTH), :]
         # if there's no command, use the previous one
         if cur_commands.shape[0] < 1:
             cur_command = prev_command
@@ -58,6 +84,6 @@ if __name__ == '__main__':
 
     # debug prints
     print(counter, new_navdata.shape, joined_data.shape)
-    np.save("joined_data", joined_data)
-    np.save("new_navdata", new_navdata)
+    np.save("joined_data_long_reduced_%d_%d" % (WINDOW_LENGTH, NAVDATA_OFFSET_MILIS), joined_data)
+    np.save("new_navdata_long_reduced_%d_%d" % (WINDOW_LENGTH, NAVDATA_OFFSET_MILIS), new_navdata)
 
